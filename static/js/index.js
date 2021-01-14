fe8be823585ac0b7eb1ef3d04c992026b8a963ed
Andy Situ <and.situ@gmail.com>
2021-01-13 21:58:18 -0800

    Update + set top promo bar

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,9 @@
-console.log("test");
+window.onload = function() {
+  document.addEventListener('scroll', function(e) {
+    if (document.body.scrollTop > 300) {
+      document.getElementById("promo-nav-container").style.marginTop = "-42px";
+    } else {
+      document.getElementById("promo-nav-container").style.marginTop = "0px";
+    }
+  });
+};
