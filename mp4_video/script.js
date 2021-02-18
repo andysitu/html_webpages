aa19d7f695c62f17b168264ad375ec2a49982f61
Andy Situ <and.situ@gmail.com>
2021-02-17 22:17:03 -0800

    Add mp4_video

---
+++ mp4_video/script.js
@@ -0,0 +1,6 @@
+// Set div to width & height of browser
+window.onload = function() {
+  const div = document.getElementById("video-container");
+  div.style.height = window.innerHeight + "px";
+  div.style.width = window.innerWidth + "px";
+}
